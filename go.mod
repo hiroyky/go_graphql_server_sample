23bb12d7de6713b04c6e0f44b8b28606f05fe81d
Hirokazu Yokoyama <hiroyky.voyager@gmail.com>
2021-11-28 12:40:49 +0900

    dataloader

--- go.mod
+++ go.mod
@@ -12,4 +12,9 @@ require (
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.0 // indirect
 	github.com/mitchellh/mapstructure v0.0.0-20180203102830-a4e142e9c047 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/vektah/dataloaden v0.3.0 // indirect
+	golang.org/x/mod v0.3.0 // indirect
+	golang.org/x/tools v0.0.0-20210106214847-113979e3529a // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- server.go
+++ server.go
@@ -3,10 +3,13 @@ package main
 import (
 	"context"
 	"github.com/99designs/gqlgen/graphql"
+	"github.com/hiroyky/go_graphql_server_sample/dataloader"
+	"github.com/hiroyky/go_graphql_server_sample/graph/model"
 	"github.com/vektah/gqlparser/v2/gqlerror"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
@@ -22,7 +25,23 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	companyLoader := dataloader.NewCompanyLoader(dataloader.CompanyLoaderConfig{
+		MaxBatch: 100,
+		Wait:     2 * time.Millisecond,
+		Fetch: func(keys []string) ([]*model.Company, []error) {
+			companies := make([]*model.Company, len(keys))
+			errors := make([]error, len(keys))
+
+			// 取得処理を書く
+
+			// 引数のkeysに対応する順番の配列で返す。
+			return companies, errors
+		},
+	})
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{
+		CompanyLoader: companyLoader,
+	}}))
 	// エラー処理を書く
 	srv.SetErrorPresenter(func(ctx context.Context, e error) *gqlerror.Error {
 		err := graphql.DefaultErrorPresenter(ctx, e)
